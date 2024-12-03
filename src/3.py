8cb0d6beb6ae83d12fd81bacff85f7d23cf3bc9b
MatasJurev <matukas.jurevicius@gmail.com>
2024-12-03 22:07:38 +0200

    3.2

--- src/3.py
+++ src/3.py
@@ -13,7 +13,28 @@ def part_one():
 
 
 def part_two():
-    lines = fileutils.read_lines("data/3.txt")
+    inp = ""
+    for line in fileutils.read_lines("data/3.txt"):
+        inp += line
+    
+    matches = re.findall(r'(mul\((\d+),(\d+)\))|(do\(\))|(don\'t\(\))', inp)
+    ans = 0
+    mul_enabled = True
+
+    for match in matches:
+        for el in match:
+            if 'mul' in el and mul_enabled:
+                a, b = map(int, el.replace("mul(", "").replace(")", "").split(","))
+                ans += a * b
+                break
+            elif el == "do()":
+                mul_enabled = True
+                break
+            elif el == "don't()":
+                mul_enabled = False
+                break
+    
+    print(ans)
 
 
 def main():
