9be58d805035fb22f368fb6e5d3f9332c025a7c9
MatasJurev <matukas.jurevicius@gmail.com>
2024-12-05 23:05:03 +0200

    4draft

--- src/4.py
+++ src/4.py
@@ -2,32 +2,51 @@ import fileutils
 
 
 def part_one():
-    lines = fileutils.read_lines("data/4.txt")
-    n = len(lines) # Both x and y are 140
-    print(lines, n)
-    print(lines[139][139])
-    word = ""
+    lines = fileutils.read_lines("data/4simple.txt")
+    n = 4
     total = 0
+    len_lines = len(lines)
+    print(lines)
 
     #construct a string of 4 adjacent elements either horizontally, vertically or diagonally
-    for i in range(n):
-        for j in range(n):
-            #horizontal
-            if j < n - 3:
-                word = lines[i][j:j+4]
-            #vertical
-            if i < n - 3:
-                word = lines[i][j] + lines[i+1][j] + lines[i+2][j] + lines[i+3][j]
-            #diagonal
-            if i < n - 3 and j < n - 3:
-                word = lines[i][j] + lines[i+1][j+1] + lines[i+2][j+2] + lines[i+3][j+3]
-            if i < n - 3 and j > 2:
-                word = lines[i][j] + lines[i+1][j-1] + lines[i+2][j-2] + lines[i+3][j-3]
-            if word == "XMAS" or word == "SAMX":
-                total += 1
+    # for i in range(len_lines):
+    #     for j in range(len_lines):
+    #         words = []
+    #         #horizontal
+    #         words.append(lines[i][j:j+4])
+    #         #vertical
+    #         words.append(lines[i][j] + lines[i+1][j] + lines[i+2][j] + lines[i+3][j])
+    #         #diagonal
+    #         words.append(lines[i][j] + lines[i+1][j+1] + lines[i+2][j+2] + lines[i+3][j+3])
+    #         words.append(lines[i][j] + lines[i+1][j-1] + lines[i+2][j-2] + lines[i+3][j-3])
+            
+    #         for word in words:
+    #             if word == "XMAS" or word == "SAMX":
+    #                 total += 1
+
+    for i in range(len_lines):
+        for j in range(len_lines):
+            words = []
+            # Horizontal
+            if j + n <= len_lines:
+                words.append([lines[i][j + i] for i in range(n)])
+            # Vertical
+            if i + n <= len_lines:
+                words.append([lines[i + i][j] for i in range(n)])
+            # Diagonal (down-right)
+            if i + n <= len_lines and j + n <= len_lines:
+                words.append([lines[i + i][j + i] for i in range(n)])
+            # Diagonal (up-right)
+            if i - n + 1 >= 0 and j + n <= len_lines:
+                words.append([lines[i - i][j + i] for i in range(n)])
+            
+            for word in words:
+                word = ''.join(word)
+                if word == "XMAS" or word == "SAMX":
+                    total += 1
 
     print(total)
-                
+
 
 def part_two():
     lines = fileutils.read_lines("data/4.txt")
